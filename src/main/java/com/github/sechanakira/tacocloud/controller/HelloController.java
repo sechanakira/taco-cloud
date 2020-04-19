72db14d59d114131234a0c73eabe790baa67edb4
Shingirai Chanakira <sechanakira@yahoo.com>
2020-04-19 13:20:29 +0200

    Home Controller

---
+++ src/main/java/com/github/sechanakira/tacocloud/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.sechanakira.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/com/github/sechanakira/tacocloud/controller/HelloControllerTest.java
@@ -0,0 +1,10 @@
+package com.github.sechanakira.tacocloud.controller;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+    @Test
+    void home() {
+    }
+}
