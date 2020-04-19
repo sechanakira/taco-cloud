1d5a35ab61dd28f8a2dd117206d0b9159a8e0c34
Shingirai Chanakira <sechanakira@yahoo.com>
2020-04-19 14:27:52 +0200

    Home Controller

---
+++ src/main/java/com/github/sechanakira/tacocloud/model/Ingredient.java
@@ -0,0 +1,17 @@
+package com.github.sechanakira.tacocloud.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
